a4a780d31e784f3661c8e2cebd14b7bef5a1de63
JoelDickel <64815075+JoelDickel@users.noreply.github.com>
2023-04-18 20:13:15 -0300

    Subindo teste

---
+++ Teste/Teste/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿
+namespace Teste
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Botão = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Botão
+            // 
+            this.Botão.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Botão.Location = new System.Drawing.Point(294, 166);
+            this.Botão.Name = "Botão";
+            this.Botão.Size = new System.Drawing.Size(189, 136);
+            this.Botão.TabIndex = 0;
+            this.Botão.Text = "Botão";
+            this.Botão.UseVisualStyleBackColor = true;
+            this.Botão.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label1.Location = new System.Drawing.Point(291, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(208, 79);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Titudo da página";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 461);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Botão);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.button1_Click);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Botão;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Teste/Teste/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Teste
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            
+            
+
+            InitializeComponent();
+            //this.Text = "Joel Dickel";
+            //this.Size = new Size(1000, 1000);
+            //this.ControlBox = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = "Joel Dickel";
+        }
+
+
+    }
+}
